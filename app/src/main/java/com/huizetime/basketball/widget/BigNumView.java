330ea09ea14fe17b80abffa344d6faed6e25e67d
water_fairy <995637517@qq.com>
2016-09-27 21:08:33 +0800

    传输框架

--- app/src/main/java/com/huizetime/basketball/widget/BigNumView.java
+++ app/src/main/java/com/huizetime/basketball/widget/BigNumView.java
@@ -13,12 +13,10 @@ import java.util.List;
  */
 public class BigNumView extends LinearLayout {
     private Context mContext;
-    private List<NumView> mList;//numView 的数量
+    private List<NumView> mList;//numView 集合
     private int mItemNum;//数量
-    private int mNum;//数字
-    private int mLastNum;
-    private int mDivide;
-    private int[] resIds;
+    private int mLastNum;//上一个数字
+    private int[] resIds;//图片资源id
     private AttributeSet attrs;
 
     public BigNumView(Context context) {
@@ -38,21 +36,22 @@ public class BigNumView extends LinearLayout {
     }
 
     public void setNum(int num, boolean change) {
-        this.mNum = num;
         if (num >= Math.pow(10, mItemNum)) {
             Log.i("bigNumView", "超过最大值");
             num = (int) (Math.pow(10, mItemNum) - 1);
         }
-//110  200
-        String strNum = num + "";
-        String lastStrNum = mLastNum + "";
 
-        int len = strNum.length();
-        int priLen = lastStrNum.length();
+        String strNum = num + "";//即将展示的数据 转为字符串
+        String lastStrNum = mLastNum + "";//之前数据
 
+        int len = strNum.length();//即将展示的数据长度
+        int priLen = lastStrNum.length();//之前数据
+        //拆分数字
         for (int i = 0; i < mItemNum; i++) {
-            int tempNum = 0;
+
+            int tempNum = 0;//
             int priTempNum = 0;
+            //从个位开始
             if (len >= i + 1) {
                 String temp1 = strNum.charAt(len - i - 1) + "";
                 tempNum = Integer.parseInt(temp1);
@@ -61,6 +60,7 @@ public class BigNumView extends LinearLayout {
                 String temp2 = lastStrNum.charAt(priLen - i - 1) + "";
                 priTempNum = Integer.parseInt(temp2);
             }
+            //相对应的数字切换 相同不再切换
             if (tempNum != priTempNum) {
                 mList.get(mItemNum - i - 1).setNum(tempNum, priTempNum, change);
             }
@@ -69,8 +69,13 @@ public class BigNumView extends LinearLayout {
         mLastNum = num;
     }
 
+    /**
+     * 设置数字位数
+     *
+     * @param itemNum 位数
+     * @param divide  分割距离
+     */
     public void setItemNum(int itemNum, int divide) {
-        this.mDivide = divide;
         this.mItemNum = itemNum;
         for (int i = 0; i < itemNum; i++) {
             NumView numView = new NumView(mContext, attrs);
@@ -86,6 +91,10 @@ public class BigNumView extends LinearLayout {
         }
     }
 
+    /**
+     * 设置资源id   0-9
+     * @param resIds
+     */
     public void setResId(int[] resIds) {
         this.resIds = resIds;
         setNum(0, false);
--- app/src/main/java/com/huizetime/basketball/widget/NumView.java
+++ app/src/main/java/com/huizetime/basketball/widget/NumView.java
@@ -18,12 +18,10 @@ import android.widget.ScrollView;
  */
 public class NumView extends ScrollView implements View.OnTouchListener {
     private static final String TAG = "numView";
-    private int[] resIds;
-    private LinearLayout linearLayout;
-    private ImageView priImg, img;
-    private int width, height;
-    private int num;//0~9
-    private boolean change;
+    private int[] resIds;//资源ids
+    private LinearLayout linearLayout;//数字插入的位置 垂直布局
+    private ImageView priImg, img;//上图,下图
+    private int num;//0~9  设置显示的数据
 
     public NumView(Context context) {
         super(context);
@@ -46,17 +44,30 @@ public class NumView extends ScrollView implements View.OnTouchListener {
         img = new ImageView(context);
         linearLayout.addView(priImg);
         linearLayout.addView(img);
-//        setNum(0, false);
     }
 
+    /**
+     * 设置数字
+     *
+     * @param num
+     * @param change 是否有动画
+     */
     public void setNum(int num, boolean change) {
         setNum(num, -1, change);
     }
 
+    /**
+     * 设置数字
+     *
+     * @param num    现在的数字
+     * @param priNum 前一个数字
+     * @param change 是否有动画
+     */
     public void setNum(int num, int priNum, boolean change) {
         this.num = num;
-
+//若priNum==-1  前一个数字默认设置为  num-1
         if (priNum == -1) {
+            //当前为0 ,前一个为9
             if (num == 0) {
                 priNum = 9;
             } else {
@@ -74,6 +85,7 @@ public class NumView extends ScrollView implements View.OnTouchListener {
         }
     }
 
+    //动画 after(false)
     private void anim() {
         TranslateAnimation animation = new TranslateAnimation(
                 Animation.RELATIVE_TO_SELF, 0,
@@ -94,13 +106,6 @@ public class NumView extends ScrollView implements View.OnTouchListener {
         }
     };
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        width = w;
-        height = h;
-        Log.i(TAG, "onSizeChanged: " + height);
-    }
 
     public NumView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
